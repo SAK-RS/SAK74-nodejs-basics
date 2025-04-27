f8338509acace2778351fd063561dc9ce9b42e2f
SAK74 <sergijks@gmail.com>
2025-04-27 12:01:11 +0200

    feat: implement stream and Zlib tasks

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,14 @@
+import { join } from "node:path";
+import { createReadStream } from "node:fs";
+import { stdout } from "node:process";
+
 const read = async () => {
-    // Write your code here 
+  const path = join(import.meta.dirname, "files", "fileToRead.txt");
+  const readStream = createReadStream(path, { encoding: "utf-8" });
+  readStream.pipe(stdout);
+  readStream.on("end", () => {
+    stdout.write("\n");
+  });
 };
 
 await read();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,14 @@
+import { createGunzip } from "node:zlib";
+import { join } from "node:path";
+import { chdir } from "node:process";
+import { createReadStream, createWriteStream } from "node:fs";
+
 const decompress = async () => {
-    // Write your code here 
+  chdir(join(import.meta.dirname, "files"));
+  const gunzip = createGunzip();
+  const readStream = createReadStream("archive.gz");
+  const writeStream = createWriteStream("fileToCompress.txt");
+  readStream.pipe(gunzip).pipe(writeStream);
 };
 
 await decompress();
